20c60901a85dcafd558b31bcd199a1ebd089404b
ShivdasJadhav <jshivdas07@gmail.com>
2024-01-14 19:12:13 +0530

    updated with access origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: "https://shivs-buzz.vercel.app"
+    origin: ["https://shivs-buzz.vercel.app","https://nextpractices-5pfmxi0qo-shivdasjadhav.vercel.app"]
   })
   await app.listen(3001);
 }

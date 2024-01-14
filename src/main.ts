15f24a074e9213930f0f40d836cda998d2b232ef
ShivdasJadhav <jshivdas07@gmail.com>
2024-01-14 19:23:38 +0530

    updated with access origin

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ["https://buzz-client.vercel.app/"]
-  })
+  app.enableCors()
   await app.listen(3001);
 }
 bootstrap();

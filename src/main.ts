9ef9bde0e47f662fffce73e20d959affdcc8c3be
Shivdas Jadhav <83746444+ShivdasJadhav@users.noreply.github.com>
2024-01-15 13:07:25 +0530

    Update main.ts
    cors errror solved

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: ["https://buzz-client.netlify.app"],
+    origin: "*",
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true
   })

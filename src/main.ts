823f76af36d841c416d96b16a18908e3ef0c56b5
ShivdasJadhav <jshivdas07@gmail.com>
2024-01-15 12:48:34 +0530

    cors error check 10

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: ["*", "http://localhost:3000", "https://buzz-client.netlify.app"],
+    origin: ["https://buzz-client.netlify.app"],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true
   })
